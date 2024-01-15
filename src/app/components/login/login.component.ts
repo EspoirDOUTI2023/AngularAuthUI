d9fbc2c538cde792eca299fc5d6f7e7e2387c569
EspoirDOUTI2023 <murphyjunior82@gmail.com>
2024-01-15 23:13:26 +0100

    Add login page + validators

--- src/app/components/login/login.component.ts
+++ src/app/components/login/login.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { FormBuilder, FormGroup, Validators, FormControl } from '@angular/forms';
+import ValidateForm from 'src/app/helpers/validateForm';
 
 @Component({
   selector: 'app-login',
@@ -6,5 +8,26 @@ import { Component } from '@angular/core';
   styleUrls: ['./login.component.scss']
 })
 export class LoginComponent {
+  loginForm!: FormGroup;
+  constructor(private fb : FormBuilder){ }
+
+  ngOnInit(): void {
+    this.loginForm = this.fb.group({
+      email : ['', Validators.compose([Validators.required, Validators.email])],
+      password : ['', Validators.required]
+    })
+  }
+
+  onSubmit(){
+    if(this.loginForm.valid){
+      //send data to backend
+      console.log(this.loginForm.value)
+    }else{
+      //throw the error using toaster and with required fields
+      ValidateForm.validateAllFormFields(this.loginForm);
+      // alert('Your form is invalid')
+    }
+  }
+
 
 }
